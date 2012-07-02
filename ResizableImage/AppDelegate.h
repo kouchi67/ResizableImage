b12386f7eccb412ecdbdcf3b3ab99c95af08461b
niyaty <hiroking0607@gmail.com>
2012-07-02 22:54:22 +0900

    Initial Commit

---
+++ ResizableImage/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ResizableImage
+//
+//  Created by Hiroki Kouchi on 12/07/02.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ResizableImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ResizableImage
+//
+//  Created by Hiroki Kouchi on 12/07/02.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
