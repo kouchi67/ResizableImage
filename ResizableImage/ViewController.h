91bfbe059ade6577049813523ad8772ccc23a73d
niyaty <hiroking0607@gmail.com>
2012-07-03 00:04:42 +0900

    add backgroundImage for UIButton.

--- ResizableImage/ViewController.h
+++ ResizableImage/ViewController.h
@@ -9,5 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *button;
+@property (weak, nonatomic) IBOutlet UISlider *sliderTop;
+@property (weak, nonatomic) IBOutlet UISlider *sliderLeft;
+@property (weak, nonatomic) IBOutlet UISlider *sliderBottom;
+@property (weak, nonatomic) IBOutlet UISlider *sliderRight;
 
 @end
--- ResizableImage/ViewController.m
+++ ResizableImage/ViewController.m
@@ -9,19 +9,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+- (void)configureButtonImage;
 @end
 
 @implementation ViewController
+{
+    UIImage *image;
+}
+
+@synthesize button;
+@synthesize sliderTop;
+@synthesize sliderLeft;
+@synthesize sliderBottom;
+@synthesize sliderRight;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    image = [UIImage imageNamed:@"button.png"];
+    [self configureButtonImage];
 }
 
 - (void)viewDidUnload
 {
+    [self setSliderTop:nil];
+    [self setSliderLeft:nil];
+    [self setSliderBottom:nil];
+    [self setSliderRight:nil];
+    [self setButton:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -29,6 +45,42 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (void)configureButtonImage
+{
+    CGFloat top = 0.0;
+    CGFloat left = 10.0;
+    CGFloat bottom = 0.0;
+    CGFloat right = 10.0;
+    
+//    top = sliderTop.value;
+//    left = sliderLeft.value;
+//    bottom = sliderBottom.value;
+//    right = sliderRight.value;
+    
+    UIImage *resizedImage = [image resizableImageWithCapInsets:UIEdgeInsetsMake(top, left, bottom, right)];
+    [button setBackgroundImage:resizedImage forState:UIControlStateNormal];
+}
+
+- (IBAction)valueChangeTop:(id)sender
+{
+    [self configureButtonImage];
+}
+
+- (IBAction)valueChangeLeft:(id)sender
+{
+    [self configureButtonImage];
+}
+
+- (IBAction)valueChangeBottom:(id)sender
+{
+    [self configureButtonImage];
+}
+
+- (IBAction)valueChangeRight:(id)sender
+{
+    [self configureButtonImage];
 }
 
 @end
